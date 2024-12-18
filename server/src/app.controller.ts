b08560dd5d7ddb9dd5f52d476be5636170ce416d
slince-zero <1711257704@qq.com>
2024-12-18 16:12:16 +0800

    refactor: server init

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ClerkGuard } from './clerk.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('users')
-  @UseGuards(ClerkGuard)
+  @Get('')
   async getUsers() {
     return this.appService.getUsers();
   }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { clerkClient } from '@clerk/express';
 
 @Injectable()
 export class AppService {
   async getUsers(): Promise<any> {
-    return clerkClient.users.getUserList();
+    return 'hello world';
   }
 }
