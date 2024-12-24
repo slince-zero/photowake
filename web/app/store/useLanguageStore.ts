8caef33e6b1df75384b91da2baf9c2041688a707
slince-zero <1711257704@qq.com>
2024-12-24 11:32:08 +0800

    fix

--- web/app/store/useLanguageStore.ts
+++ web/app/store/useLanguageStore.ts
@@ -1,19 +1,10 @@
 'use client'
 import { create } from 'zustand'
 
-// 获取浏览器语言
-const getBrowserLang = () => {
-  if (typeof window !== 'undefined') {
-    // 获取浏览器语言，只取前两位，由 zh-CN 变为 zh
-    return navigator.language.slice(0, 2)
-  }
-  return 'en'
-}
-
 export const useLanguageStore = create<{
   currentLang: string
   setCurrentLang: (lang: string) => void
 }>((set) => ({
-  currentLang: getBrowserLang(),
+  currentLang: 'en',
   setCurrentLang: (lang) => set({ currentLang: lang }),
 }))
