76fb7afb744086d56f4fcc64e88cd9208e14d445
slince-zero <1711257704@qq.com>
2025-01-03 17:05:18 +0800

    fix

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: join(process.cwd(), '.env'),
-      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      ignoreEnvFile: true,
+      isGlobal: true,
     }),
   ],
   controllers: [AppController],
